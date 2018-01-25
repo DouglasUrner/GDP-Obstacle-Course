c2e58d613e5160320f20919f8d4f88b41b385c0d
Douglas Urner <dlu@canishe.com>
2018-01-25 12:18:18 -0800

    Initial commit of Unity files.

---
+++ ObstacleCourse/Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 1f;
+
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+		rb.AddForce(speed * Input.GetAxis("Horizontal"), speed * Input.GetAxis("Vertical"), 0);
+	}
+
+	private void OnCollisionEnter(Collision other) {
+		Debug.Log("OnCollisionEnter(): " + name + " collided with " + other.gameObject.name);
+		Destroy(other.gameObject);
+	}
+}
