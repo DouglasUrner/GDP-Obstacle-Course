1a75ef72a3de77a63158680a5ccd45788e34ae3a
Douglas Urner <dlu@canishe.com>
2018-01-25 12:30:05 -0800

    Add obstacle controller script.

--- ObstacleCourse/Assets/ObstacleController.cs
+++ ObstacleCourse/Assets/ObstacleController.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObstacleController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 		
-	}
+	public GameObject obstaclePrefab;
+	public float spawnDist = 3f;
 	
-	// Update is called once per frame
-	void Update () {
+	private bool quitting = false;
+	private GameObject player;
+
+	void Start () {
+		player = GameObject.Find("Player");
+	}
+
+	private void OnApplicationQuit() {
+		quitting = true;
+	}
+
+	private void OnDestroy() {
+		Debug.Log(name + ".OnDestory()");
 		
+		if (!quitting) {
+			var newObstacle = Instantiate(obstaclePrefab);
+			var playerPosition = player.transform.position;
+			newObstacle.transform.position = playerPosition + new Vector3(Random.RandomRange(0f, spawnDist), Random.RandomRange(0f, spawnDist), 0);
+			newObstacle.tag = "Obstacle";
+			newObstacle.SetActive(true);
+		}
 	}
 }
